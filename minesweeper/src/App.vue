3f45d7094aba32d94f17d77549841e44e22296bb
DariaG16 <113132247+DariaG16@users.noreply.github.com>
2023-04-15 18:40:02 -0400

    f

---
+++ minesweeper/src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import tile from "./components/tile.vue";
+</script>
+
+<template>
+  <body>
+    <header><h1>Minesweeper</h1></header>
+    <div><tile @click="reveal" v-for="item in items" :key="item"></tile></div>
+  </body>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [],
+      methods: {
+        reveal() {
+          //check if there is a bomb
+          //if there is: function gameover
+          //if there isn't: remove btn
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: #ff9770;
+  font-size: 60px;
+  text-align: center;
+  margin: 50px auto;
+  margin-top: 50px;
+  color: #ff70a6;
+  width: 600px;
+  text-shadow: 3px 1px #63458a;
+  border: solid #e9ff70 5px;
+}
+div {
+  background-color: #ffd670;
+  width: 650px;
+  height: 80vh;
+  margin: 30px auto;
+  border: solid #e9ff70 5px;
+}
+</style>
---
+++ minesweeper/src/components/tile.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="reveal"></button>
+</template>
+
+<script>
+export default {
+  name: "tile",
+};
+</script>
+
+<style scoped>
+button {
+  width: 30px;
+  height: 30px;
+}
+</style>
